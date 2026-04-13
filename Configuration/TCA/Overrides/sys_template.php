a7c2bd7c2c9971a2d0773d101c6060c2c8ce83c3
Olivier <osc@archriss.com>
2026-04-13 17:22:28 +0200

    [TASK] Compatibility for TYPO3 13

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,3 +1,5 @@
 <?php
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('private_download', 'Configuration/TypoScript', 'Private Download');
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+ExtensionManagementUtility::addStaticFile('private_download', 'Configuration/TypoScript', 'Private Download');
